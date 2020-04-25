c1d4c66a45da156cff46a8c5fa0a47553dfe6ab1
Hugo Flores <hfp.ing2@gmail.com>
2020-04-25 18:14:43 -0400

    Custom Marker class

---
+++ TouchToMap/Views/Components/CustomPointAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  CustomPointAnnotation.swift
+//  TouchToMap
+//
+//  Created by Hugo Flores Perez on 4/25/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class CustomPointAnnotation: MKPointAnnotation {
+    static let reuseIdentifier = "MapAnnotation"
+    var pinCustomImageName:String!
+}
