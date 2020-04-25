4ae94a81d0da2059e3622552ede82e7a22bb1b01
Hugo Flores <hfp.ing2@gmail.com>
2020-04-25 19:47:54 -0400

    Location Details View

---
+++ TouchToMap/Views/Screens/LocationDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  LocationDetailsView.swift
+//  TouchToMap
+//
+//  Created by Hugo Flores Perez on 4/25/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
