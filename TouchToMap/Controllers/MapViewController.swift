5548c1c85cf9522709164931e0a92aa7e829c93b
Hugo Flores <hfp.ing2@gmail.com>
2020-04-25 19:48:11 -0400

    Draggable drawer effect impl

--- TouchToMap/Controllers/MapViewController.swift
+++ TouchToMap/Controllers/MapViewController.swift
@@ -13,6 +13,17 @@ import UIKit
 class MapViewController: UIViewController {
 
     private let viewObj = MapView()
+    
+    private var infoView: DraggableView?
+    var visualEffect: UIVisualEffectView?
+    let cardHeight: CGFloat = 600
+    let cardHandleAreaHeight: CGFloat = 60
+    var cardVisible = false
+    var nextCardState: DraggableView.ViewState {
+        return cardVisible ? DraggableView.ViewState.collapsed : DraggableView.ViewState.expanded
+    }
+    var runningAnimations = [UIViewPropertyAnimator]()
+    var animationProgressWhenInterrupted: CGFloat = .zero
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +33,7 @@ class MapViewController: UIViewController {
     func setUpMain() {
         view.backgroundColor = UIColor(named: "MapControllerBackground")
         setUpView()
+        setUpCard()
     }
     
     func setUpView() {
@@ -32,6 +44,89 @@ class MapViewController: UIViewController {
         viewObj.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         viewObj.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         viewObj.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func setUpCard() {
+        visualEffect = UIVisualEffectView()
+        visualEffect?.isUserInteractionEnabled = false
+        visualEffect?.frame = view.frame
+        view.addSubview(visualEffect!)
+        infoView = DraggableView()
+        addChild(infoView!)
+        view.addSubview(infoView!.view)
+        infoView!.view.frame = CGRect(x: 0, y: view.frame.height - cardHandleAreaHeight, width: view.bounds.width, height: cardHeight)
+        infoView?.view.clipsToBounds = true
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleCardTap(recognizer:)))
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(MapViewController.handleCardPan(recognizer:)))
+        infoView?.view.addGestureRecognizer(tapGestureRecognizer)
+        infoView?.view.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc
+    func handleCardTap(recognizer: UITapGestureRecognizer) {
+        
+    }
+    
+    @objc
+    func handleCardPan(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            // Start Transition
+            startInteractiveTransition(state: nextCardState, duration: 0.9)
+        case .changed:
+            // Update Transition
+            let translation = recognizer.translation(in: infoView?.view)
+            var fractionCompleted = translation.y / cardHeight
+            fractionCompleted = cardVisible ? fractionCompleted : -fractionCompleted
+            updateInteractiveTransition(fractionCompleted: fractionCompleted)
+        case .ended:
+            // Continue transition
+            continueInteractiveTransition()
+        default:
+            print("Not implemented")
+        }
+    }
+    
+    func animateTransitionIfNeeded(state: DraggableView.ViewState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                switch state {
+                case .expanded:
+                    self.infoView?.view.frame.origin.y = self.view.frame.height - self.cardHeight
+                default:
+                    self.infoView?.view.frame.origin.y = self.view.frame.height - self.cardHandleAreaHeight
+                }
+            }
+            frameAnimator.addCompletion { _ in
+                self.cardVisible = !self.cardVisible
+                self.runningAnimations.removeAll()
+            }
+            frameAnimator.startAnimation()
+            runningAnimations.append(frameAnimator)
+        }
+    }
+    
+    func startInteractiveTransition(state: DraggableView.ViewState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            animateTransitionIfNeeded(state: state, duration: duration)
+        }
+        for animator in runningAnimations {
+            animator.pauseAnimation()
+            animationProgressWhenInterrupted = animator.fractionComplete
+        }
+    }
+    
+    func updateInteractiveTransition(fractionCompleted: CGFloat) {
+        for animator in runningAnimations {
+            animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
+        }
+    }
+    
+    func continueInteractiveTransition() {
+        for animator in runningAnimations {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
     }
     
     @objc
