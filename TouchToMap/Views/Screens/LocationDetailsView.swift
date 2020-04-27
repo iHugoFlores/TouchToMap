50365c80e80249ace1f54ba609e5e23a9dd2a19d
Hugo Flores <hfp.ing2@gmail.com>
2020-04-27 09:31:34 -0400

    Details view layout and data set

--- TouchToMap/Views/Screens/LocationDetailsView.swift
+++ TouchToMap/Views/Screens/LocationDetailsView.swift
@@ -6,17 +6,16 @@
 //  Copyright © 2020 Hugo Flores Perez. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class LocationDetailsView: UIView {
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.numberOfLines = 2
-        return label
+    private let contentText: UITextView = {
+        let text = UITextView()
+        text.isUserInteractionEnabled = false
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
     }()
 
     override init(frame: CGRect) {
@@ -30,15 +29,46 @@ class LocationDetailsView: UIView {
     
     func setUpMain() {
         //insetsLayoutMarginsFromSafeArea = false
+        backgroundColor = .white
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .green
-        addSubview(titleLabel)
-        titleLabel.text = "Location details view"
+        addSubview(contentText)
         let margins = layoutMarginsGuide
-        titleLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        contentText.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        contentText.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        contentText.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        contentText.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+    }
+    
+    func setLocationData(_ mark: CLPlacemark) {
+        //print(mark.subAdministrativeArea, mark.subLocality)
+        let stringContent = NSMutableAttributedString()
+        if let locality = mark.locality, let country = mark.country {
+            stringContent.append(NSAttributedString(
+                string: "\(locality) - \(country)\n",
+                attributes: [
+                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 28)
+                ])
+            )
+        }
+        if let administrativeArea = mark.administrativeArea, let subAdministrativeArea = mark.subAdministrativeArea {
+            stringContent.append(NSAttributedString(
+                string: "\(administrativeArea) - \(subAdministrativeArea)\n",
+                attributes: [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18),
+                    NSAttributedString.Key.foregroundColor: UIColor.systemGray
+                ])
+            )
+        }
+        if let name = mark.name {
+            stringContent.append(NSAttributedString(
+                string: name + "\n",
+                attributes: [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18)
+                ])
+            )
+        }
+        
+        contentText.attributedText = stringContent
     }
     
 }
